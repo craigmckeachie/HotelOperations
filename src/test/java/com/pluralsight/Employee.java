1fd020c7bb6e17b9b78b5482224c59ce36627d5f
Craig McKeachie <craigdmckeachie@gmail.com>
2025-10-21 10:25:55 -0400

    Employee class

---
+++ src/test/java/com/pluralsight/Employee.java
@@ -0,0 +1,43 @@
+package com.pluralsight;
+
+public class Employee {
+    private int employeeId;
+    private String name;
+    private String department;
+    private double payRate;
+    private double hoursWorked;
+
+    private double REGULAR_HOURS = 40;
+    private final double OVERTIME_PAY_RATE = 1.15;
+
+
+//    public double getTotalPay() {
+//        return (getRegularHours() * payRate) + (getOvertimeHours() * OVERTIME_PAY_RATE);
+//    }
+
+    public double getTotalPay() {
+        double regularPay = getRegularHours() * payRate;
+        double overtimePay = getOvertimeHours() * OVERTIME_PAY_RATE;
+        return regularPay + overtimePay;
+    }
+
+
+    public double getRegularHours() {
+        if (hoursWorked < REGULAR_HOURS) {
+            return hoursWorked;
+        } else {
+            return REGULAR_HOURS;
+        }
+    }
+
+    public double getOvertimeHours() {
+        if (hoursWorked > REGULAR_HOURS) {
+            double overtimeHours = hoursWorked - REGULAR_HOURS;
+            return overtimeHours;
+        } else {
+            return 0;
+        }
+    }
+
+
+}
