a69131fc0c00cc6bd9bd33b549891cc61bf2071c
Craig McKeachie <craigdmckeachie@gmail.com>
2025-10-21 14:27:47 -0400

    bookRoom implement only basic room

--- src/test/java/com/pluralsight/Hotel.java
+++ src/test/java/com/pluralsight/Hotel.java
@@ -1,17 +1,46 @@
 package com.pluralsight;
 
 public class Hotel {
+    //field members, backing fields/variables
     private String name;
     private int numberOfSuites;
     private int numberOfRooms;
     private int bookedSuites;
     private int bookedBasicRooms;
 
+    //constructors
     public Hotel(String name, int numberOfSuites, int numberOfRooms) {
         this.name = name;
         this.numberOfSuites = numberOfSuites;
         this.numberOfRooms = numberOfRooms;
         this.bookedSuites = 0;
         this.bookedBasicRooms = 0;
+    }
+
+    public Hotel(String name, int numberOfSuites, int numberOfRooms, int bookedSuites, int bookedBasicRooms) {
+        this.name = name;
+        this.numberOfSuites = numberOfSuites;
+        this.numberOfRooms = numberOfRooms;
+        this.bookedSuites = bookedSuites;
+        this.bookedBasicRooms = bookedBasicRooms;
+    }
+
+    //getters
+    public int getAvailableSuites(){
+        return numberOfSuites - bookedSuites;
+    }
+
+    public int getAvailableRooms(){
+        return numberOfRooms - bookedBasicRooms;
+    }
+
+
+    public boolean bookRoom(int numberOfRooms, boolean isSuite){
+        if (getAvailableRooms() >= numberOfRooms) {
+            bookedBasicRooms = bookedBasicRooms + numberOfRooms; //OR bookedBasicRooms += numberOfRooms;
+            return true;
+        }else{
+            return false;
+        }
     }
 }
